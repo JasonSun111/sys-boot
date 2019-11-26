a3e9194c9aaa1837dcc0a24601d955f300283e18
sunys <yusen.sun@perfectek.com>
2019-11-26 20:24:22 +0800

    增加http接口

--- sys-facade/src/main/java/com/sunys/facade/annotation/Request.java
+++ sys-facade/src/main/java/com/sunys/facade/annotation/Request.java
@@ -1,17 +1,17 @@
 package com.sunys.facade.annotation;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.PARAMETER})
-@Documented
-public @interface ServiceParam {
+import com.sunys.facade.http.HttpMethod;
 
-	String value();
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Request {
+
+	String url();
 	
-	boolean require() default true;
+	HttpMethod method() default HttpMethod.GET;
 }
---
+++ sys-facade/src/main/java/com/sunys/facade/http/HttpMethod.java
@@ -0,0 +1,10 @@
+package com.sunys.facade.http;
+
+public enum HttpMethod {
+
+	GET,
+	POST,
+	PUT,
+	DELETE,
+	;
+}
